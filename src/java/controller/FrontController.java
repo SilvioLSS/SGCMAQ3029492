1374475bbe93c36327eb0ae5ce95b422994de963
silvinls <silvioluizs.santos@gmail.com>
2025-10-22 22:52:08 -0300

    aula de sessoes e cookies

--- src/java/controller/FrontController.java
+++ src/java/controller/FrontController.java
@@ -1,146 +1,160 @@
 package controller;
 
-import LogTrack.ExceptionLogTrack;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import logtrack.ExceptionLogTrack;
 import model.TipoUsuario;
 import model.Usuario;
 
-public class FrontController extends HttpServlet {
+public class FrontController extends HttpServlet {  
 
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {        
+        
         String task = request.getParameter("task");
-        if (task == null) {
+        if( task == null ) {
             task = "";
         }
-
+        
         try {
-
+        
             switch (task) {
 
-                case "tipousuario":
-                    doGetTipoUsuario(request, response);
-                    break;
+                case "tipousuario": doGetTipoUsuario(request, response); break;
 
-                case "usuarios":
-                    doGetUsuarios(request, response);
-                    break;
+                case "usuarios": doGetUsuarios(request, response); break;
+                
+                case "logout": doGetLogout(request, response); break;
 
-                default:
-                    doDefault(request, response);
+                default: doDefault(request, response);
 
             }
-
-        } catch (Exception ex) {
+        
+        } catch(Exception ex) {
             ExceptionLogTrack.getInstance().addLog(ex);
         }
-
+        
     }
+    
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String task = request.getParameter("task");
-        if (task == null) {
+        if( task == null ) {
             task = "";
         }
-
+        
         try {
-
+        
             switch (task) {
 
-                case "tipousuario":
-                    doPostTipoUsuario(request, response);
-                    break;
+                case "tipousuario": doPostTipoUsuario(request, response); break;
 
-                case "usuarios":
-                    doPostUsuarios(request, response);
-                    break;
+                case "usuarios": doPostUsuarios(request, response); break;
+                
+                case "login": doPostLogin(request, response); break;
 
-                default:
-                    doDefault(request, response);
+                default: doDefault(request, response);
 
             }
-
-        } catch (Exception ex) {
+        
+        } catch(Exception ex) {
             ExceptionLogTrack.getInstance().addLog(ex);
         }
-
+        
     }
-
+    
+    
+    
     private void doGetTipoUsuario(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        
         String action = request.getParameter("action");
-        if ((action != null)
-                && action.equals("delete")) {
-
-            int id = Integer.valueOf(request.getParameter("id"));
-
+        if( ( action != null ) &&
+                action.equals("delete") ) {
+            
+            int id = Integer.valueOf( request.getParameter("id") );
+            
             TipoUsuario tp = new TipoUsuario();
             tp.setId(id);
-
+            
             tp.delete();
         }
-
-        response.sendRedirect(request.getContextPath() + "/home/app/tipousuario.jsp");
-
+        
+        response.sendRedirect( request.getContextPath() + "/home/app/tipousuario.jsp");
+        
     }
-
+    
     private void doGetUsuarios(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        
         String action = request.getParameter("action");
-        if ((action != null)
-                && action.equals("delete")) {
-
-            int id = Integer.valueOf(request.getParameter("id"));
-
+        if( ( action != null ) &&
+                action.equals("delete") ) {
+            
+            int id = Integer.valueOf( request.getParameter("id") );
+            
             Usuario us = new Usuario();
             us.setId(id);
-
+            
             us.delete();
-
+            
         }
-
-        response.sendRedirect(request.getContextPath() + "/home/app/usuarios.jsp");
-
+        
+        response.sendRedirect( request.getContextPath() + "/home/app/usuarios.jsp");
+        
     }
+    
+    private void doGetLogout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        
+       HttpSession sessao = request.getSession(false);
+       if( sessao != null ) {
+//           sessao.removeAttribute("usuario");
+//           sessao.removeAttribute("tipo_usuario");
 
+            sessao.invalidate();
+       }
+       
+       response.sendRedirect("home/login.jsp");
+        
+    }
+    
+    
+    
     private void doPostTipoUsuario(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        
         String action = request.getParameter("action");
 
-        int id = Integer.valueOf(request.getParameter("id"));
-
+        int id = Integer.valueOf( request.getParameter("id") );
+        
         String nome = request.getParameter("nome");
-
+        
         String moduloAdministrativo = request.getParameter("modulo_administrativo");
-        if (moduloAdministrativo == null) {
+        if( moduloAdministrativo == null ) {
             moduloAdministrativo = "N";
         }
-
+        
         String moduloAgendamento = request.getParameter("modulo_agendamento");
-        if (moduloAgendamento == null) {
+        if( moduloAgendamento == null ) {
             moduloAgendamento = "N";
         }
-
+        
         String moduloAtendimento = request.getParameter("modulo_atendimento");
-        if (moduloAtendimento == null) {
+        if( moduloAtendimento == null ) {
             moduloAtendimento = "N";
         }
-
+        
         // Java Bean
         TipoUsuario tp = new TipoUsuario();
 
         tp.setId(id);
 
-        if (action.equals("update")) {
-            tp.load();
-        }
+        if( action.equals("update") ) tp.load();
 
         tp.setNome(nome);
         tp.setModuloAdministrativo(moduloAdministrativo);
@@ -148,32 +162,30 @@ public class FrontController extends HttpServlet {
         tp.setModuloAtendimento(moduloAtendimento);
 
         tp.save();
-
-        response.sendRedirect(request.getContextPath() + "/home/app/tipousuario.jsp");
-
+        
+        response.sendRedirect( request.getContextPath() + "/home/app/tipousuario.jsp");
+        
     }
-
-    private void doPostUsuarios(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+    
+    private void doPostUsuarios(HttpServletRequest request, HttpServletResponse response) throws Exception {  
+        
         String action = request.getParameter("action");
 
-        int id = Integer.valueOf(request.getParameter("id"));
-
+        int id = Integer.valueOf( request.getParameter("id") );
+        
         String nome = request.getParameter("nome");
-
+        
         String cpf = request.getParameter("cpf");
-
+        
         String senha = request.getParameter("senha");
-
-        int tipoUsuarioId = Integer.valueOf(request.getParameter("tipo_usuario_id"));
-
+        
+        int tipoUsuarioId = Integer.valueOf( request.getParameter("tipo_usuario_id") );
+        
         Usuario us = new Usuario();
 
         us.setId(id);
 
-        if (action.equals("update")) {
-            us.load();
-        }
+        if( action.equals("update") ) us.load();
 
         us.setNome(nome);
         us.setCpf(cpf);
@@ -181,15 +193,72 @@ public class FrontController extends HttpServlet {
         us.setTipoUsuarioId(tipoUsuarioId);
 
         us.save();
-
-        response.sendRedirect(request.getContextPath() + "/home/app/usuarios.jsp");
-
-    }
-
+        
+        response.sendRedirect( request.getContextPath() + "/home/app/usuarios.jsp");
+        
+    } 
+    
+    private void doPostLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {  
+        
+        int id = Integer.valueOf( request.getParameter("id") );        
+        String senha = request.getParameter("senha");
+        
+        Usuario usuarioTry = new Usuario();
+        usuarioTry.setId(id);
+        usuarioTry.setSenha(senha);
+        
+        Usuario usuario = new Usuario();
+        usuario.setId(id);        
+        boolean status = usuario.load();
+        
+        if( ( status == true ) &&
+              ( usuario.getSenha().equals( usuarioTry.getSenha() ) ) ) {
+            
+            // true crie um sessão se não houver alguma, false do contrário
+            // informações amarmazenadas no servidor
+            HttpSession sessao = request.getSession(false);
+            if( sessao != null ) {
+                // se a sessão já existir
+                sessao.invalidate();
+            }
+            
+            sessao = request.getSession(true);
+            
+            sessao.setAttribute( "usuario", "(" + usuario.getNome() + ", " + usuario.getId() + ")" );
+            
+            TipoUsuario tipoUsuario = new TipoUsuario();
+            tipoUsuario.setId( usuario.getTipoUsuarioId() );
+            tipoUsuario.load();
+            
+            sessao.setAttribute( "tipo_usuario", tipoUsuario );
+            
+            sessao.setMaxInactiveInterval( 60 * 60 ); // em segundos
+            
+            // criado e armazenado no cliente
+            Cookie cookie = new Cookie( "id", String.valueOf(id) );
+            cookie.setMaxAge( 60 * 10 ); // em segundos
+            response.addCookie(cookie);
+            
+            response.sendRedirect("home/app/menu.jsp");
+            
+        } else {
+            
+            request.setAttribute("msg", "id e/ou senha incorreto(s)");
+            request.getRequestDispatcher( "/home/login.jsp" ).forward(request, response);
+            
+        }
+        
+        
+    } 
+        
+    
+        
     private void doDefault(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        response.sendRedirect("home/login.jsp");
-
+        
+        response.sendRedirect( "home/login.jsp");
+        
     }
+    
 
+    
 }
